b4fab52f87464b6eb06715e1f778537a734a7209
Milton Honji <factorysolutionIT@hotmail.com>
2020-05-02 18:10:44 -0300

    Variáveis e um pouco sobre Type Script.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root></app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+    //Criando uma variável
+    public todos: any[] = []; //[] (vázio)
+    public title: String='Minhas Tarefas';
+
+    constructor() {
+      this.todos.push('passear com o cachorro');
+      this.todos.push('ir ao supermercado');
+      this.todos.push('cortar o cabelo');
+    }
+
 }
